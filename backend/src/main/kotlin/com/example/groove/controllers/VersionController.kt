c89e765029faf0c4ab6c1028485466015dd030e0
Ayrton Stout <AyrtonStout@outlook.com>
2019-01-24 20:36:00 -0700

    Add files I totally didn't forget about in the last commit

---
+++ backend/src/main/kotlin/com/example/groove/controllers/VersionController.kt
@@ -0,0 +1,14 @@
+package com.example.groove.controllers
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("api/version")
+class VersionController {
+
+	@GetMapping
+	fun login(): ResponseEntity<Map<String, String>> {
+		return ResponseEntity.ok(mapOf("version" to "1.0.0"))
+	}
+}
---
+++ frontend/src/components/site-wrapper/site-wrapper.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {MusicProvider} from "../../services/music-provider";
+import {Slide, toast, ToastContainer} from "react-toastify";
+import {PageRouter} from "..";
+import {Api} from "../../api";
+
+export class SiteWrapper extends React.Component {
+
+	componentDidMount() {
+		Api.get('version').then((serverVersion) => {
+			const clientVersion = "1.0.0";
+			if (serverVersion.version !== clientVersion) {
+				toast.info("Your Gorilla Groove is out of date. Please hard-reload your web page.")
+			}
+		})
+	}
+
+	render() {
+		return (
+			<MusicProvider>
+				<ToastContainer autoClose={5000} hideProgressBar={true} transition={Slide}/>
+				<PageRouter/>
+			</MusicProvider>
+		)
+	}
+}
