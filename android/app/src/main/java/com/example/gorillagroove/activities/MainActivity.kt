af7ba0918e9c459beb6b9d83f0f4d243a2a6036c
David Hunsaker <david.e.hunsaker@gmail.com>
2019-11-08 23:23:36 -0700

    UX Improvements

--- android/app/src/main/java/com/example/gorillagroove/activities/MainActivity.kt
+++ android/app/src/main/java/com/example/gorillagroove/activities/MainActivity.kt
@@ -10,9 +10,9 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.example.gorillagroove.R
-import com.example.gorillagroove.adapters.PlaylistAdapter
 import com.example.gorillagroove.db.GroovinDB
 import com.example.gorillagroove.db.model.User
 import com.example.gorillagroove.db.repository.UserRepository
@@ -47,19 +47,25 @@ class MainActivity : AppCompatActivity(), AuthenticationVolley,
         userName = response["username"].toString()
         email = response["email"].toString()
 
+        findViewById<TextView>(R.id.tv_nav_header).text = userName
+
         launch {
             withContext(Dispatchers.IO) {
                 user = repository.findUser(email)
 
-                if (user == null) {
-                    repository.createUser(userName, email, token)
-                } else repository.updateToken(user!!.id, token)
+                if (user != null) {
+                    repository.updateToken(user!!.id, token)
+                } else repository.createUser(userName, email, token)
             }
         }
         Log.i(
             "Main Activity",
             "What's up dude, we just snagged ourselves some token: $token and userName: $userName"
         )
+
+        emailField.text.clear()
+        passwordField.text.clear()
+        emailField.requestFocus()
     }
 
     override fun onLogoutResponse(response: JSONObject) {
@@ -76,8 +82,10 @@ class MainActivity : AppCompatActivity(), AuthenticationVolley,
         emailField = findViewById(R.id.editText2)
         passwordField = findViewById(R.id.editText)
 
-        if(intent.hasExtra("token")) token = intent.getStringExtra("token")
-        if(intent.hasExtra("username")) token = intent.getStringExtra("username")
+        emailField.requestFocus()
+
+        if (intent.hasExtra("token")) token = intent.getStringExtra("token")
+        if (intent.hasExtra("username")) token = intent.getStringExtra("username")
 
         val toggle = ActionBarDrawerToggle(
             this,
@@ -136,7 +144,6 @@ class MainActivity : AppCompatActivity(), AuthenticationVolley,
                 }
             }
         }
-
         return when (item.itemId) {
             R.id.action_settings_logout -> true
             else -> super.onOptionsItemSelected(item)
@@ -154,11 +161,15 @@ class MainActivity : AppCompatActivity(), AuthenticationVolley,
                 startActivity(intent)
             }
             R.id.nav_playlists -> {
-                val intent = Intent(applicationContext, PlaylistActivity::class.java)
-                intent.putExtra("token", token)
-                intent.putExtra("username", userName)
-                intent.putExtra("email", email)
-                startActivity(intent)
+                if (token.isNotBlank()) {
+                    val intent = Intent(applicationContext, PlaylistActivity::class.java)
+                    intent.putExtra("token", token)
+                    intent.putExtra("username", userName)
+                    intent.putExtra("email", email)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@MainActivity, "You must be logged in to go here!", Toast.LENGTH_LONG).show()
+                }
             }
         }
 
