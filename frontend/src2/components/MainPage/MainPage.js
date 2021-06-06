2e49de28977548efb129fbff300c9ad880a0eaaf
audrey <audreywiltsie@outlook.com>
2021-06-06 13:42:20 -0600

    add an init state

--- frontend/src2/components/MainPage/MainPage.js
+++ frontend/src2/components/MainPage/MainPage.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {withRouter} from 'react-router-dom';
 import {deleteCookie} from '../../../src/cookie';
 import {Api} from '../../util/api';
 import Logo from '../Logo';
+import {getDeviceIdentifier} from '../../util';
 
 
 const MainPage = ({history}) => {
+	const [isInitializing, setIsIntializing] = useState(true);
+
 	const handleLogOut = async (event) => {
 		event.preventDefault();
 		try {
@@ -19,6 +22,41 @@ const MainPage = ({history}) => {
 			history.push('/login');
 		}
 	};
+
+	const handleInit = async () => {
+		// this is where we will get all the data.
+		// 1. load user data
+		//   a. permissions - if a request fails, redirect to login
+		const permissionsResponse = await Api.get('user/permissions');
+		console.log(permissionsResponse);
+		//   b. device data
+		const deviceResponse = await Api.get('device/active?excluding-device=' + getDeviceIdentifier());
+		console.log(deviceResponse)
+		// 2. load user's music data
+		//   a. library
+		//   b. playlists
+		//   c. playlist mappings
+		//   d. review queues
+		setIsIntializing(false);
+	}
+
+	useEffect(() => {
+		if (isInitializing) {
+			// show init message 1 sec + actual load time
+			setTimeout(handleInit, 1000);
+		}
+	}, [isInitializing])
+
+	if (isInitializing) {
+		return (
+			<div className={'init'}>
+				<img src={'../../images/logo.png'} alt={'logo'}/>
+				<p>
+					Initializing Gorillas...
+				</p>
+			</div>
+		)
+	}
 
 	return (
 		<div className={'main'}>
